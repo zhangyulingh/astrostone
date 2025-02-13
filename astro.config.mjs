0e24d1f43f87e7567f96f5360247445451e8a9a9
zhangyulin <yulin.zhang@camtg.cn>
2025-02-13 16:33:21 +0800

    feat：update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clever-choux-88a621.netlify.app/',
+  integrations: [preact()],
 })
